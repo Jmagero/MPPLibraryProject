ccfb67804b0d14756d47a7ee4daa532dc64af972
Jocyline Magero <jmagero54@gmail.com>
2023-01-26 10:44:24 -0600

    Add Exception

---
+++ MPPLibraryProject/src/domain/exception/LibrarySystemException.java
@@ -0,0 +1,17 @@
+package domain.exception;
+
+import java.io.Serializable;
+
+public class LibrarySystemException extends Exception implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	public LibrarySystemException() {
+		super();
+	}
+	public LibrarySystemException(String msg) {
+		super(msg);
+	}
+	public LibrarySystemException(Throwable t) {
+		super(t);
+	}
+}
