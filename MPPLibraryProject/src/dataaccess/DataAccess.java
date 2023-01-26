2b1eaae5400f04241d58c556c3340905488ca5e8
Jocyline Magero <jmagero54@gmail.com>
2023-01-26 10:42:31 -0600

    Update Dataaccess

--- MPPLibraryProject/src/dataaccess/DataAccess.java
+++ MPPLibraryProject/src/dataaccess/DataAccess.java
@@ -2,12 +2,17 @@ package dataaccess;
 
 import java.util.HashMap;
 
+import domain.Author;
 import domain.Book;
 import domain.LibraryMember;
+
 
 public interface DataAccess { 
 	public HashMap<String,Book> readBooksMap();
 	public HashMap<String,User> readUserMap();
 	public HashMap<String, LibraryMember> readMemberMap();
-	public void saveNewMember(LibraryMember member); 
+	public HashMap<String,Author> readAuthorMap();
+	public void saveNewMember(LibraryMember member);
+	public void saveNewBook(Book book); 
+	public void updateBookHM(HashMap<String, Book> hmBooks);
 }
